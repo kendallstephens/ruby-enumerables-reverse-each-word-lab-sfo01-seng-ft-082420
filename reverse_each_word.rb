e08a39b7cc2d6265901406535a015f5d833c2e9f
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-09 15:31:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ def reverse_each_word(sentence)
   new_array = []
   sentence_array = sentence.split("")
  if sentence_array.each do {|element| element.reverse!}
-   new_array << sentence
+
 end
 end
